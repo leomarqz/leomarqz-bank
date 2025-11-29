977c0ff9637cd1299b1ee8a168b1e044c63e011b
Elmer Marquez <leomarqz.main@gmail.com>
2025-11-29 16:10:45 -0600

    feat: se creo IRoleRepository usando JPA

---
+++ src/main/java/com/leomarqz/bank/role/repository/IRoleRepository.java
@@ -0,0 +1,12 @@
+package com.leomarqz.bank.role.repository;
+
+import com.leomarqz.bank.role.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface IRoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
+
+}
