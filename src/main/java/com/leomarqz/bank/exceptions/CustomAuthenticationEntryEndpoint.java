5dc95b9f98383ea801c362531d55906d18c29bb2
Elmer Marquez <leomarqz.main@gmail.com>
2025-12-04 14:23:23 -0600

    feat: se agregaron CustomAccessDenialHandler y CustomAuthenticationEntryEndpoint

---
+++ src/main/java/com/leomarqz/bank/exceptions/CustomAuthenticationEntryEndpoint.java
@@ -0,0 +1,40 @@
+
+package com.leomarqz.bank.exceptions;
+
+import com.leomarqz.bank.res.Response;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationEntryEndpoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException )
+            throws IOException, ServletException
+    {
+        Response<?> errorResponse = Response.builder()
+                .statusCode(HttpStatus.UNAUTHORIZED.value()) //401
+                .message( authException.getMessage() )
+                .build();
+
+        response.setContentType("application/json");
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+    }
+
+}
